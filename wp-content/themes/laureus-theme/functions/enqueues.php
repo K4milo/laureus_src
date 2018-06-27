630c77437bd4e80823532c604bb53f5d5ac9fb87
k4milo <kmilo.vanegas@gmail.com>
2018-06-27 05:11:17 -0500

    Estructura inicial de compilación

--- wp-content/themes/laureus-theme/functions/enqueues.php
+++ wp-content/themes/laureus-theme/functions/enqueues.php
@@ -4,26 +4,17 @@ function laureus_enqueues() {
 
 	/* Styles */
 
-	wp_register_style('bootstrap-css', get_template_directory_uri() . '/layoutcss/vendors/bootstrap.css', false, APP_ASSETS_VERSION, null);
+	wp_register_style('bootstrap-css', get_template_directory_uri() . '/css/vendors/bootstrap.css', false, APP_ASSETS_VERSION, null);
 	wp_enqueue_style('bootstrap-css');
 
-	wp_register_style('font-awesome-css', get_template_directory_uri() . '/layoutcss/vendors/font-awesome.css', false, APP_ASSETS_VERSION, null);
-	wp_enqueue_style('font-awesome-css');
-
-	wp_register_style('fancybox-css', get_template_directory_uri() . '/layoutcss/vendors/fancybox.css', false, APP_ASSETS_VERSION, null);
+	wp_register_style('fancybox-css', get_template_directory_uri() . '/css/vendors/fancybox.css', false, APP_ASSETS_VERSION, null);
 	wp_enqueue_style('fancybox-css');
 
-	wp_register_style('reset-css', get_template_directory_uri() . '/layoutcss/reset.css', false, APP_ASSETS_VERSION, null);
+	wp_register_style('reset-css', get_template_directory_uri() . '/css/reset.css', false, APP_ASSETS_VERSION, null);
 	wp_enqueue_style('reset-css');
 
-	wp_register_style('fonts-css', get_template_directory_uri() . '/layoutcss/fonts.css', false, APP_ASSETS_VERSION, null);
-	wp_enqueue_style('fonts-css');
-
-	wp_register_style('general-css', get_template_directory_uri() . '/layoutcss/general.css', false, APP_ASSETS_VERSION, null);
-	wp_enqueue_style('general-css');
-
-	wp_register_style('mq-css', get_template_directory_uri() . '/layoutcss/mq.css', false, APP_ASSETS_VERSION, null);
-	wp_enqueue_style('mq-css');
+	wp_register_style('styles-css', get_template_directory_uri() . '/css/styles.css', false, APP_ASSETS_VERSION, null);
+	wp_enqueue_style('styles-css');
 
 
 
@@ -44,8 +35,6 @@ function laureus_enqueues() {
 	wp_register_script('general', get_template_directory_uri() . '/js/general.js', false, wp_register_script, true);
 	wp_enqueue_script('general');
 
-	wp_register_script('map', get_template_directory_uri() . '/js/map.js', false, wp_register_script, true);
-	wp_enqueue_script('map');
 
 	if (is_singular() && comments_open() && get_option('thread_comments')) {
 		wp_enqueue_script('comment-reply');
