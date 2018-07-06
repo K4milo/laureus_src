dff4e993796ef889d18a95113118de2abe704a4a
k4milo <kmilo.vanegas@gmail.com>
2018-07-06 17:30:49 -0500

    Ultimos ajustes pre deploy

--- wp-content/themes/laureus-theme/includes/template-parts/home/block-home-involved.php
+++ wp-content/themes/laureus-theme/includes/template-parts/home/block-home-involved.php
@@ -25,13 +25,18 @@
 				$involved_icon = get_sub_field('involved_icon');
 				$involved_title_item = get_sub_field('involved_title_item');
 				$involved_item_link = get_sub_field('involved_item_link');
+				$target = get_sub_field('new_tab');
+				$url_destination = "";
+				if($target == true){
+					$url_destination = "_blank";
+				}
 		?>
 			<li style="background-image: url('<?php echo $involved_image_item; ?>')">
-				<a href="<?php echo $involved_title_link; ?>" title="<?php echo $involved_title_item; ?>"></a>
+				<a href="<?php echo $involved_item_link; ?>" title="<?php echo $involved_title_item; ?>" target="<?php echo $url_destination; ?>"></a>
 				<div class="involved__wrap">
 					
 					<img src="<?php echo $involved_icon; ?>" alt="Icon <?php echo $involved_title_item; ?>">
-					<h4><?php echo $involved_title_item; ?></h4>
+					<a href="<?php echo $involved_item_link; ?>" title="<?php echo $involved_title_item; ?>" target="<?php echo $url_destination; ?>"><h4><?php echo $involved_title_item; ?></h4></a>
 					
 				</div>
 			</li>
